24403043b18ec141842847db8f0c5226ad211a96
Olga Curylo <curylo.olga@gmail.com>
2020-11-17 10:11:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ function happyHolidays() { return "Happy holidays!"};
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
